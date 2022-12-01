528c97bbb4bc4a587bb96288fa50660624b93448
Griffin Davis <contact@griffindvs.com>
2022-11-30 18:32:28 -0600

    add data and sankey

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,48 @@
-const TEST_DATA_LOCATION = '/data/test.json'
+import data from '../data/budget.json';
+  
+am4core.useTheme(am4themes_animated);
 
-async function loadData() {
-    try {
-        const file = await fetch(TEST_DATA_LOCATION);
-        console.log('Data loaded!');
-        // TODO: Process data
-        return file;
-    } catch (e) {
-        console.error("Could not load JSON from path", TEST_DATA_LOCATION);
-        throw new Error('Could not load data.');
+var chart = am4core.create("chartdiv", am4charts.SankeyDiagram);
+chart.data = data;
+chart.dataFields.fromName = "from";
+chart.dataFields.toName = "to";
+chart.dataFields.value = "value";
+
+// for right-most label to fit
+chart.paddingRight = 100;
+chart.paddingBottom = 25;
+
+// Add events on links
+chart.links.template.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+chart.links.template.events.on("hit", function(ev) {
+    chart.colors.reset();
+    var nav = chart.children.getIndex(0);
+    var linkData = ev.target.dataItem.dataContext;
+    if (linkData.sub) {
+    chart.data = linkData.sub;
+    nav.data.push({
+        name: ev.target.populateString("{fromName}→{toName}"),
+        step: ev.target.dataItem.dataContext
+    });
+    nav.invalidateData();
     }
-}
+});
+
+// Add navigation bar
+var nav = chart.createChild(am4charts.NavigationBar);
+nav.data = [{ name: "Top" }];
+nav.toBack();
+
+nav.links.template.events.on("hit", function(ev) {
+    var target = ev.target.dataItem.dataContext;
+    var nav = ev.target.parent;
+    chart.colors.reset();
+    if (target.step) {
+    chart.data = target.step.sub;
+    nav.data.splice(nav.data.indexOf(target) + 1);
+    nav.invalidateData();
+    } else {
+    chart.data = data;
+    nav.data = [{ name: "Top" }];
+    }
+});
